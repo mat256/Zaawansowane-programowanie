84759b05cbaf36cf340682ae37396a26212a01f0
Mateusz <mateusz.nabaglo@edu.uekat.pl>
2021-11-21 17:19:11 +0100

    dodano zadania 2-6

---
+++ zad_4.py
@@ -0,0 +1,5 @@
+def sum_and_compare(x: int, y: int, z: int) -> bool:
+    return x + y >= z
+
+
+print(sum_and_compare(2, 1, 4))
---
+++ zad_5.py
@@ -0,0 +1,8 @@
+def value_in_list(lista: list, x: int) ->bool:
+    return (x in lista)
+
+
+lista= [1,2,3,4]
+v=7
+
+print(value_in_list(lista,v))
