e0f7d49181bc42e981b100abc5b24f0f86bfe8af
Mateusz <mateusz.nabaglo@edu.uekat.pl>
2022-01-14 22:57:27 +0100

    fix main.py

--- detection/main.py
+++ detection/main.py
@@ -15,18 +15,19 @@ def detection(file: str):
 
     # using a greyscale picture, also for faster detection
     # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    boxes, weights = hog.detectMultiScale(frame, winStride=(3,3),
+    boxes, weights = hog.detectMultiScale(frame, winStride=(3, 3),
                                           padding=(8, 8))
 
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
     boxes = non_max_suppression(boxes, probs=None, overlapThresh=0.63)
 
-    #print(len(boxes))
+    # print(len(boxes))
     for (xA, yA, xB, yB) in boxes:
         # display the detected boxes in the colour picture
         cv2.rectangle(frame, (xA, yA), (xB, yB),
                       (0, 255, 0), 2)
-    cv2.putText(frame, "Detected: "+str(len(boxes)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.FONT_HERSHEY_SIMPLEX)
+    cv2.putText(frame, "Detected: " + str(len(boxes)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
+                cv2.FONT_HERSHEY_SIMPLEX)
     cv2.imshow('frame', frame)
 
     cv2.waitKey(0)
@@ -35,11 +36,10 @@ def detection(file: str):
 
 
 def test():
-    t="ppl1.jpg ppl2.jpg ppl3.jpg ppl4.jpg ppl5.jpg"
+    t = "ppl1.jpg ppl2.jpg ppl3.jpg ppl4.jpg ppl5.jpg"
     for a in t.split():
         detection(a)
 
 
-#detection("ppl5.jpg")
+# detection("ppl5.jpg")
 test()
-
